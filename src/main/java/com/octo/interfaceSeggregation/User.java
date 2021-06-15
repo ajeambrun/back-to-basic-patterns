41d6dd88677df5a5144bc04b78eaae10a6b9720c
Alexandre Jeambrun <alexandre.jeambrun@octo.com>
2021-06-15 11:41:53 +0200

    step 2

---
+++ src/main/java/com/octo/interfaceSeggregation/User.java
@@ -0,0 +1,13 @@
+package com.octo.interfaceSeggregation;
+
+public class User {
+    String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
--- src/main/java/com/octo/interfaceSeggregation/UserService.java
+++ src/main/java/com/octo/interfaceSeggregation/UserService.java
@@ -2,4 +2,8 @@ package com.octo.interfaceSeggregation;
 
 public interface UserService {
     String getUsername(int userId);
+
+    String getUsername(User user);
+
+    User getUsr(int userId);
 }
